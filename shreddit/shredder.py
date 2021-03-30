bc954affab8ec37c9136e9b1858d3294820ba093
Harshad Dange <WickedLynx@users.noreply.github.com>
2021-03-30 10:53:21 +0200

    Update shredder.py
    WIP

--- shreddit/shredder.py
+++ shreddit/shredder.py
@@ -125,8 +125,8 @@ class Shredder(object):
 
         self._logger.debug("Editing and deleting {msg}".format(msg=msg))
         self._logger.info("Waiting {} seconds and continuing...".format(self._batch_cooldown))
-            time.sleep(self._batch_cooldown)
         if not self._trial_run:
+            time.sleep(self._batch_cooldown)
             comment.edit(replacement_text)
 
     def _remove(self, item):
